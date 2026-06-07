2e5184ba8261ed17cb47cdfeca08b8e14f998bfd
sandro-sa <sandrosa0315@outlook.com>
2026-06-07 15:47:46 -0300

    Update routes

--- portal-noticias/src/app/app.routes.ts
+++ portal-noticias/src/app/app.routes.ts
@@ -6,35 +6,35 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./public/layout/public-layout.component')
         .then(m => m.PublicLayoutComponent),
-//     children: [
-//       {
-//         path: '',
-//         loadComponent: () =>
-//           import('./public/pages/home/home-page.component')
-//             .then(m => m.HomePageComponent),
-//       },
-//       {
-//         path: 'noticias',
-//         loadComponent: () =>
-//           import('./public/pages/category-list/category-list-page.component')
-//             .then(m => m.CategoriesListPageComponent),
-//       },
-//       {
-//         path: 'noticias/:slug',
-//         loadComponent: () =>
-//           import('./public/pages/category-page/category-page.component')
-//             .then(m => m.CategoryPageComponent),
-//       },
-//       {
-//         path: 'noticia/:slug',
-//         loadComponent: () =>
-//           import('./public/pages/notice-page/notice-page.component')
-//             .then(m => m.NoticePageComponent),
-//       },
-//     ],
-//   },
-//   {
-//     path: '**',
-//     redirectTo: '',
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadComponent: () =>
+  //         import('./public/pages/home/home-page.component')
+  //           .then(m => m.HomePageComponent),
+  //     },
+  //     {
+  //       path: 'noticias',
+  //       loadComponent: () =>
+  //         import('./public/pages/category-list/category-list-page.component')
+  //           .then(m => m.CategoriesListPageComponent),
+  //     },
+  //     {
+  //       path: 'noticias/:slug',
+  //       loadComponent: () =>
+  //         import('./public/pages/category-page/category-page.component')
+  //           .then(m => m.CategoryPageComponent),
+  //     },
+  //     {
+  //       path: 'noticia/:slug',
+  //       loadComponent: () =>
+  //         import('./public/pages/notice-page/notice-page.component')
+  //           .then(m => m.NoticePageComponent),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '',
   },
 ];
